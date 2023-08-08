0ceb031ec33d0e38f5e7fd7f7e7934dd8f0f6415
adam buchanan <141166018+1039814@users.noreply.github.com>
2023-08-08 16:06:55 +0000

    08_08_23

---
+++ 08_07_23/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    //properties (fields)
+    public String Name = "Fluffy";
+    public int age = 3;
+    public String breed = "Siamese";
+    public String favoritefood = "Tuna";
+
+    //behavors Methods
+    public void displaycat(){
+        System.out.println("Name: " + Name);
+        System.out.println("age: " + age);
+        System.out.println("breed:" + breed);
+        System.out.println("favoritefood:" +favoritefood);
+    }
+
+}
---
+++ 08_08_23/Smile.java
@@ -0,0 +1,13 @@
+class smile{
+    public static void main(String[] args) {
+        
+        System.out.println("  oooo  ");
+        System.out.println(" o    o ");
+        System.out.println("o o  o o");
+        System.out.println("o      o");
+        System.out.println("o o  o o");
+        System.out.println("o  oo  o");
+        System.out.println(" o    o ");
+        System.out.println("  oooo  ");
+    }
+}
