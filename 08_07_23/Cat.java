9b242b34238b4a9f83dbfc992f7ac55f49abe166
TeachingSTEMisCool <116288621+TeachingSTEMisCool@users.noreply.github.com>
2023-08-08 16:06:40 +0000

    fixed Cat class

---
+++ 08_07_23/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    //Properties (fields)
+    public String name = "Fluffy";
+    public int age = 3;
+    public String breed = "Siamese";
+    public String favoriteFood = "Tuna";
+
+    //Behaviors (methods)
+    public void displayCat(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Breed: " + breed);
+        System.out.println("Favorite Food: " + favoriteFood);
+    }
+}
